c0f570cab446f82d150773d07a78edc70e875966
B-Nilson <nilson@unbc.ca>
2026-02-11 13:16:27 -0800

    document get_outdated_files()

--- R/get_outdated_files.R
+++ R/get_outdated_files.R
@@ -1,14 +1,17 @@
-get_outdated_files <- function(
-  server_file_dates,
-  local_file_dates
-) {
+#' Get paths to local files that are out of date relative to the server
+#'
+#' @param server_file_dates Data frame of server file details with columns for file path, data obervation date, and file creation date
+#' @param local_file_dates Data frame of local file details with columns for file path, data obervation date, and file creation date
+#' @return Character vector of file paths of outdated local files
+#' @export
+get_outdated_files <- function(server_file_dates, local_file_dates) {
   if (length(local_file_dates) == 0) {
     return(NULL)
   }
 
   server_file_dates |>
     dplyr::full_join(
-      local_file_dates,
+      y = local_file_dates,
       by = "observed",
       suffix = c("_server", "_local")
     ) |>
