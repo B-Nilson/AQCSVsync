4f7360e8349ed896810ce5c155429096515eee24
B-Nilson <nilson@unbc.ca>
2026-02-11 13:18:01 -0800

    document get_local_duplicates()

--- R/get_local_duplicates.R
+++ R/get_local_duplicates.R
@@ -1,3 +1,8 @@
+#' Get local files that have duplicate observation dates
+#'
+#' @param file_dates Data frame of file details with columns for file path, data observation date, and file creation date
+#' @return Character vector of file paths of all but the newest of local files with duplicates
+#' @export
 get_local_duplicates <- function(file_dates) {
   if (is.null(file_dates)) {
     return(NULL)
